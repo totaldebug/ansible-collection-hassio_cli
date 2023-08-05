d82464908ef5a4b45760c0d5ae8cd19f4f0d6517
mjkl-gh <9350991+mjkl-gh@users.noreply.github.com>
2023-08-05 13:16:58 +0200

    Fix util functions

--- plugins/module_utils/hassio_utils.py
+++ plugins/module_utils/hassio_utils.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+hassio = "ha"
+
+
 def join(*args):
     return " ".join(list(args))
 
-def execute_core(ansible, action, token=None):
+
+def execute(ansible, module, action, token=None):
     token_argument = join("--api-token", token) if token is not None else ""
-    cmd = join(hassio, host, action, token_argument)
+    cmd = join(hassio, module, action, token_argument)
     return ansible.run_command(cmd)
 
-def start(ansible, token):
-    return execute_core(ansible, "start", token)
 
-def restart(ansible, token):
-    return execute_core(ansible, "restart", token)
+def start(module, ansible, token=None):
+    return execute(ansible, module, "start", token)
 
-def stop(ansible, token):
-    return execute_core(ansible, "stop", token)
 
-def update(ansible, token):
-    return execute_core(ansible, "update", token)
+def restart(module, ansible, token=None):
+    return execute(ansible, module, "restart", token)
 
-def join(*args):
-    return " ".join(list(args))
 
-def update(ansible, token):
-    cmd = join(hassio, host, "update", "--api-token", token)
-    return ansible.run_command(cmd)
+def stop(module, ansible, token=None):
+    return execute(ansible, module, "stop", token)
+
+
+def update(module, ansible, token=None):
+    return execute(ansible, module, "update", token)
+
 
 def __raise(ex):
     raise ex
