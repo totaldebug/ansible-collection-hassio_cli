b462d183285b2464256c840851af480238322357
mjkl-gh <9350991+mjkl-gh@users.noreply.github.com>
2023-08-06 22:22:07 +0200

    Make os use partial function

--- plugins/modules/hassio_os.py
+++ plugins/modules/hassio_os.py
@@ -33,6 +33,7 @@ EXAMPLES = """
 #
 import json
 import traceback
+from functools import partial
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_native
@@ -44,12 +45,16 @@ from ansible_collections.totaldebug.hassio_cli.plugins.module_utils.hassio_utils
     __raise,
 )
 
-host = "os"
+module = "os"
 
 
 def main():
+    switch = {
+        "updated": partial(update, module),
+    }
+
     module_args = dict(
-        state=dict(type="str", required=True, choices=["updated"]),
+        state=dict(type="str", required=True, choices=switch.keys()),
         token=dict(type="bool", required=False),
     )
 
@@ -58,7 +63,6 @@ def main():
         supports_check_mode=True,
     )
 
-    switch = {"updated": update}
     state = ansible_module.params["state"]
     token = ansible_module.params["token"]
 
@@ -92,66 +96,6 @@ def main():
                 result["message"] = action(ansible_module, token)
 
         ansible_module.exit_json(**result)
-    except Exception as e:
-        ansible_module.fail_json(msg=to_native(e), exception=traceback.format_exc())
-
-
-if __name__ == "__main__":
-    main()
-
-
-def main():
-    switch = {
-        "restarted": partial(restart, module),
-        "stopped": partial(stop, module),
-        "updated": partial(update, module),
-        "started": partial(start, module),
-    }
-
-    module_args = dict(
-        state=dict(type="str", required=True, choices=switch.keys()),
-        token=dict(type="bool", required=False),
-    )
-
-    ansible_module = AnsibleModule(
-        argument_spec=module_args,
-        supports_check_mode=True,
-    )
-
-    state = ansible_module.params["state"]
-    token = ansible_module.params["token"]
-
-    try:
-        facts_json = get_info(ansible_module, module, token)[1]
-        facts = json.loads(facts_json)["data"]
-
-        result = dict(changed=False, message="", ansible_facts=facts)
-
-        if state in state_and_changed:
-            result["changed"] = state_and_changed[state](facts)
-        else:
-            __raise(Exception("Check function is undefined"))
-
-        if ansible_module.check_mode:
-            ansible_module.exit_json(**result)
-
-        if facts["update_available"] is True:
-            action = switch.get(
-                state,
-                lambda ansible_module, token: __raise(Exception("Action is undefined")),
-            )
-            message = action(ansible_module, token)
-
-        if message[0] == 1:
-            result["failed"] = True
-            result["msg"] = message
-            ansible_module.fail_json(**result)
-
-        if message[0] == 0:
-            result["changed"] = True
-            result["msg"] = message
-            ansible_module.exit_json(**result)
-
     except Exception as e:
         ansible_module.fail_json(msg=to_native(e), exception=traceback.format_exc())
 
